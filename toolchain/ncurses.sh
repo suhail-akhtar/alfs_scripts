485e2ccecab7e8f7e9cf0f4b90243a223256b166
Suhail Akhtar <suhailakhtar@live.com>
2022-07-27 11:11:10 +0500

    LFS 11.1
    Updated script for LFS 11.1

--- toolchain/ncurses.sh
+++ toolchain/ncurses.sh
@@ -7,9 +7,9 @@ fi
 
 cd $LFS/sources
 
-tar xvf ncurses-6.2.tar.gz
+tar xvf ncurses-6.3.tar.gz
 
-cd  ncurses-6.2
+cd  ncurses-6.3
 
 sed -i s/mawk// configure
 
@@ -30,6 +30,7 @@ popd
             --without-debug              \
             --without-ada                \
             --without-normal             \
+            --disable-stripping          \
             --enable-widec
 
 make
@@ -37,13 +38,14 @@ make
 make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic install
 echo "INPUT(-lncursesw)" > $LFS/usr/lib/libncurses.so
 
-mv -v $LFS/usr/lib/libncursesw.so.6* $LFS/lib
+# following two lines are not part of LFS 11.1
+# mv -v $LFS/usr/lib/libncursesw.so.6* $LFS/lib
 
-ln -sfv ../../lib/$(readlink $LFS/usr/lib/libncursesw.so) $LFS/usr/lib/libncursesw.so
+# ln -sfv ../../lib/$(readlink $LFS/usr/lib/libncursesw.so) $LFS/usr/lib/libncursesw.so
 
 cd $LFS/sources
 
-rm -rf ncurses-6.2
+rm -rf ncurses-6.3
 
 echo "Done"
 
