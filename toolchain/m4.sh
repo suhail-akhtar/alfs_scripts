04b7c5e173130db3650cb18386779aeeb87cf73e
Suhail Akhtar <suhailakhtar@live.com>
2022-07-27 11:05:10 +0500

    m4 version update LFS 11.1
    Line commented:    # echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h

--- toolchain/m4.sh
+++ toolchain/m4.sh
@@ -12,7 +12,7 @@ tar xvf m4-1.4.19.tar.xz
 cd m4-1.4.19
 
 # sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
-echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
+# echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
 
 ./configure --prefix=/usr   \
             --host=$LFS_TGT \
